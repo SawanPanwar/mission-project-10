153c3eb9ac17248623cdc4239e2de144b7b2c10d
SawanPanwar <skpanwar25@gmail.com>
2025-09-17 16:05:49 +0530

    17-sept-2025

---
+++ UI/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { HttpServiceService } from './http-service.service';
+import { EndpointServiceService } from './endpoint-service.service';
+import { ServiceLocatorService } from './service-locator.service';
+import { AuthServiceService } from './auth-service.service';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [
+    HttpServiceService,
+    EndpointServiceService,
+    ServiceLocatorService,
+    AuthServiceService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ UI/src/app/base.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from "@angular/core";
+
+
+@Component({
+    template: ''
+})
+export class BaseCtl implements OnInit {
+
+    public form: any = {
+        error: false, //error 
+        message: null, //error or success message
+        preload: [], // preload data
+        data: { id: null }, //form data
+        inputerror: {}, // form input error messages
+        searchParams: {}, //search form
+        list: [], // search list 
+        pageNo: 0
+    };
+
+    public api: any = {
+        endpoint: '',
+        get: '',
+        save: '',
+        search: '',
+        deleteMany: '',
+        preload: '',
+    }
+
+    initApi(ep: any) {
+        this.api.endpoint = ep;
+        this.api.get = ep + "/get";
+        this.api.save = ep + "/save";
+        this.api.search = ep + "/search";
+        this.api.deleteMany = ep + "/deleteMany";
+        this.api.preload = ep + "/preload";
+    }
+
+    constructor(public endpoint: String) {
+        var _self = this;
+        _self.initApi(endpoint);
+    }
+
+    ngOnInit(): void {
+
+    }
+}
